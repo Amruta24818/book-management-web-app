cf98b81d9a8dcce56e4e1082c2b016d8afdb60c0
Shubham Jadhav <shubham.jadhav@vayana.com>
2023-07-27 16:03:03 +0530

    eslint setup

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "airbnb",
+    "airbnb-typescript",
+    "plugin:prettier/recommended",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json"],
+  },
+  plugins: ["@typescript-eslint", "react"],
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+        singleQuote: true,
+      },
+    ],
+    "import/prefer-default-export": "off",
+    "react/require-default-props": [0],
+    "react/function-component-definition": [
+      2,
+      { namedComponents: "arrow-function" },
+    ],
+  },
+};
